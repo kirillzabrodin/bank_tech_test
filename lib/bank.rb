333fae18e36c229f7ec73e38700cb5c9f3028254
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-25 21:55:03 +0300

    working debit and credit

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,24 @@
 class Bank
 
+  def initialize
+    @balance = 0
+    @statment = "date || credit || debit || balance\n"
+  end
+
   def status
-    return 'date || credit || debit || balance'
+    return @statment
+  end
+
+  def credit(num)
+    @balance += num
+    @statment += "#{Time.now.strftime("%d/%m/%Y %H:%M")} || #{'%.2f' % num} || || #{'%.2f' % @balance}\n"
+    return '%.2f' % @balance
+  end
+
+  def debit(num)
+    @balance -= num
+    @statment += "#{Time.now.strftime("%d/%m/%Y %H:%M")} || #{'%.2f' % num} || || #{'%.2f' % @balance}\n"
+    return '%.2f' % @balance
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,7 +5,25 @@ describe Bank do
 
   describe 'status' do
     it 'returns empty status if no transactions' do
-      expect(subject.status).to eq 'date || credit || debit || balance'
+      expect(subject.status).to eq "date || credit || debit || balance\n"
+    end
+
+    it 'returns the status and latest transation' do
+      subject.credit(1000)
+      expect(subject.status).to eq "date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y %H:%M")} || 1000.00 || || 1000.00\n"
+    end
+
+  end
+
+  describe 'credit' do
+    it 'credits the account with the amount described' do
+      expect(subject.credit(1000)).to eq '%.2f' % 1000
+    end
+  end
+
+  describe 'debit' do
+    it 'debits the account with the amount described' do
+      expect(subject.debit(1000)).to eq '%.2f' % -1000
     end
   end
 
