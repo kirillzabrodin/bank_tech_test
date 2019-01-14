cc5e27fa541ffda0f543c68eb7c83f82e28aa808
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-14 11:16:13 +0000

    timecop working

--- lib/bank.rb
+++ lib/bank.rb
@@ -11,13 +11,25 @@ class Bank
 
   def credit(num)
     @balance += num
-    @statment.prepend("#{Time.now.strftime("%d/%m/%Y %H:%M")} || #{'%.2f' % num} || || #{'%.2f' % @balance}\n")
-    return '%.2f' % @balance
+    line = "#{time} || #{format('%.2f', num)} || || #{format('%.2f', @balance)}"
+    add_to_statement(line)
+    format('%.2f', @balance)
   end
 
   def debit(num)
     @balance -= num
-    @statment.prepend("#{Time.now.strftime("%d/%m/%Y %H:%M")} || || #{'%.2f' % num} || #{'%.2f' % @balance}\n")
-    return '%.2f' % @balance
+    line = "#{time} || || #{format('%.2f', num)} || #{format('%.2f', @balance)}"
+    add_to_statement(line)
+    format('%.2f', @balance)
+  end
+
+  private
+
+  def add_to_statement(line)
+    @statment.prepend("#{line}\n")
+  end
+
+  def time
+    Time.now.strftime('%d/%m/%Y %H:%M')
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,33 +1,38 @@
 require 'bank'
+require 'timecop'
 
 describe Bank do
-  describe 'status' do
-    it 'returns empty status if no transactions' do
-      expect(subject.status).to eq "date || credit || debit || balance\n"
+  t = Time.local(2008, 9, 1, 10, 5, 0)
+  Timecop.travel(t) do
+
+    describe 'status' do
+      it 'returns empty status if no transactions' do
+        expect(subject.status).to eq "date || credit || debit || balance\n"
+      end
+
+      it 'returns the status and latest transation' do
+        subject.credit(1000)
+        expect(subject.status).to eq "date || credit || debit || balance\n#{Time.now.strftime('%d/%m/%Y %H:%M')} || 1000.00 || || 1000.00\n"
+      end
+
+      it 'returns the status and latest transation in correct order' do
+        subject.credit(10)
+        subject.credit(1000)
+        statement = "date || credit || debit || balance\n#{Time.now.strftime('%d/%m/%Y %H:%M')} || 1000.00 || || 1010.00\n#{Time.now.strftime('%d/%m/%Y %H:%M')} || 10.00 || || 10.00\n"
+        expect(subject.status).to eq statement
+      end
     end
 
-    it 'returns the status and latest transation' do
-      subject.credit(1000)
-      expect(subject.status).to eq "date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y %H:%M")} || 1000.00 || || 1000.00\n"
+    describe 'credit' do
+      it 'credits the account with the amount described' do
+        expect(subject.credit(1000)).to eq format('%.2f', 1000)
+      end
     end
 
-    it 'returns the status and latest transation in correct order' do
-      subject.credit(10)
-      subject.credit(1000)
-      statement = "date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y %H:%M")} || 1000.00 || || 1010.00\n#{Time.now.strftime("%d/%m/%Y %H:%M")} || 10.00 || || 10.00\n"
-      expect(subject.status).to eq statement
-    end
-  end
-
-  describe 'credit' do
-    it 'credits the account with the amount described' do
-      expect(subject.credit(1000)).to eq '%.2f' % 1000
-    end
-  end
-
-  describe 'debit' do
-    it 'debits the account with the amount described' do
-      expect(subject.debit(1000)).to eq '%.2f' % -1000
+    describe 'debit' do
+      it 'debits the account with the amount described' do
+        expect(subject.debit(1000)).to eq format('%.2f', -1000)
+      end
     end
   end
 end
